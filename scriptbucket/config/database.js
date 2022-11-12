27fdad67a2dd5d4b73e7f0f18c8066c4caa4fb20
Brandon EtteR <brandonetter@gmail.com>
2022-11-12 14:04:15 -0500

    jfc

--- scriptbucket/config/database.js
+++ scriptbucket/config/database.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv").config({ path: __dirname + "/../../.env" });
 console.log(dotenv);
 module.exports = {
   development: {
-    storage: process.env.DB,
+    storage: "./scriptbucket/" + process.env.DB,
     dialect: "sqlite",
   },
 };
